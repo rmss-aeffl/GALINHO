63113079bcc70d43df18759fc77754e94e267a6c
Rui Soares <rui.soares.aeffl@hotmail.com>
2021-11-08 11:57:24 +0000

    Add project files.

---
+++ GALINHO/FormJogo2Players.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GALINHO
+{
+    public partial class FormJogo2Players : Form
+    {
+        private TabuleiroJogoGalo tab;
+        int jogador = 1;
+        public FormJogo2Players()
+        {
+            InitializeComponent();
+        }
+
+        private void jogando_casa(PictureBox pictureBox)
+        {
+            int linha=1;
+            int coluna=1;
+
+            if (pictureBox.Name == "pictureBox1") { linha = 1; coluna = 1; }
+            if (pictureBox.Name == "pictureBox2") { linha = 1; coluna = 2; }
+            if (pictureBox.Name == "pictureBox3") { linha = 1; coluna = 3; }
+            if (pictureBox.Name == "pictureBox4") { linha = 2; coluna = 1; }
+            if (pictureBox.Name == "pictureBox5") { linha = 2; coluna = 2; }
+            if (pictureBox.Name == "pictureBox6") { linha = 2; coluna = 3; }
+            if (pictureBox.Name == "pictureBox7") { linha = 3; coluna = 1; }
+            if (pictureBox.Name == "pictureBox8") { linha = 3; coluna = 2; }
+            if (pictureBox.Name == "pictureBox9") { linha = 3; coluna = 3; }
+
+            if (tab.verificarCasaVazia(linha, coluna) == true)
+            {
+                if (jogador == 1)
+                    tab.jogarCasa(linha, coluna, 1);
+                else if (jogador == 2)
+                    tab.jogarCasa(linha, coluna, 2);
+                if (jogador == 1)
+                    pictureBox.Image = Image.FromFile("x.png");
+                else
+                    pictureBox.Image = Image.FromFile("o.png");
+                if (jogador == 1)
+                    jogador = 2;
+                else if (jogador == 2)
+                    jogador = 1;
+            }
+
+            label1.Text = "É a vez do jogador " + jogador.ToString() +
+                            " ( " +  ((jogador == 1) ? "X" : "O") + " )";
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox1);
+
+        }
+
+        private void FormJogo2Players_Load(object sender, EventArgs e)
+        {
+            tab = new TabuleiroJogoGalo();
+            label1.Text = "É a vez do jogador " + jogador.ToString() +
+                          " ( " + ((jogador == 1) ? "X" : "O") + " )";
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox3);
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox4);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox5);
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox6);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox7);
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox8);
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            jogando_casa(pictureBox9);
+        }
+    }
+}
---
+++ GALINHO/TabuleiroJogoGalo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GALINHO
+{
+    class TabuleiroJogoGalo
+    {
+        protected char[,] casas; // 
+        protected string ganhou; // X - 1 player O -2 player N - niguem E - empate
+
+        public TabuleiroJogoGalo()
+        {
+            ganhou = "N";
+            casas = new char[3,3]{ { 'N', 'N', 'N' },
+                                    { 'N', 'N', 'N' }, 
+                                    { 'N', 'N', 'N' } };
+            
+        }
+
+        public string testarVencedor()
+        {
+
+            /*falta desenvolver */
+            return "N";
+            return "X";
+            return "O";
+        }
+
+        public bool verificarCasaVazia(int lin, int col)
+        {
+            if (casas[lin-1,col-1]=='N') // casa vazia
+                return true;
+            return false;
+        }
+
+        public void jogarCasa(int lin, int col, int jog)
+        {
+            if (jog == 1)
+                casas[lin - 1, col - 1] = 'X';
+            if (jog == 2)
+                casas[lin - 1, col - 1] = 'O';
+        }
+
+    }
+}
